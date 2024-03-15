28074b1c22da72253ff02890bf0b523d53f1a163
mdevito4 <117404956+mdevito4@users.noreply.github.com>
2024-03-15 12:13:07 -0400

    improvement: can now filter data when fetching

--- database-project/frontend/src/App.jsx
+++ database-project/frontend/src/App.jsx
@@ -1,11 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Button, Grid, TextField } from '@mui/material';
 
 function App() {
-  const [integrationData, setIntData] = useState({});
+  const [integrationData, setIntData] = useState([]);
   const [fieldValue, setFieldValue] = useState("");
-  
-  const fetchData = async () => {
+  const [filterValue, setFilterValue] = useState("");
+
+  const fetchFitlerData = async () => {
+    let url = 'http://localhost:5050/test/';
+    if (filterValue !== "") {
+      url += `?filter=${filterValue}`;
+    }
+    await fetch(url, {
+      method: 'GET'
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (filterValue !== "") {
+          const filteredData = data.filter(item => item._id === filterValue);
+          setIntData(filteredData);
+        } else {
+          setIntData(data);
+        }
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }
+
+  const fetchAllData = async () => {
     await fetch('http://localhost:5050/test/', {
       method: 'GET'
     })
@@ -40,7 +64,7 @@ function App() {
     <Grid container spacing ={3} style={{width:'50vw', height:'75vh', marginLeft:'25vw', marginTop:'12.5vh'}}>
       <Grid item xs={8}>
         <TextField 
-        fullWidth
+          fullWidth
           variant='outlined' 
           label='Test' 
           value={fieldValue}
@@ -61,18 +85,37 @@ function App() {
           Add Data
         </Button>
       </Grid>
-      <Grid item xs={4} />
+      <Grid item xs={4}>
+        <TextField
+          fullWidth
+          variant='outlined'
+          label='Filter'
+          value={filterValue}
+          onChange={(event) => {
+            setFilterValue(event.target.value)
+          }}
+        />
+      </Grid>
       <Grid item xs={4}>
         <Button
           variant='contained'
           onClick={() => {
-            fetchData();
+            fetchFitlerData();
           }}
         >
-          Get Data
+          Get Fitler Data
         </Button>
       </Grid>
-      <Grid item xs={4} />
+      <Grid item xs={4}>
+        <Button
+          variant='contained'
+          onClick={() => {
+            fetchAllData();
+          }}
+        >
+          Get All Data
+        </Button>
+      </Grid>
       <Grid item xs={12}>
         <pre>
           {JSON.stringify(integrationData, null, 2)}
